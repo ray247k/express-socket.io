fe41ce6a7fb27f05b89d23bdf80dc735c57851ee
Ray <ray247k@gmail.com>
2020-02-15 18:22:26 +0800

    Add show online user count

--- index.js
+++ index.js
@@ -6,11 +6,16 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/view/index.html')
 });
 
+let online_user_count = 0
 io.on('connection', function (socket) {
+    online_user_count += 1
     socket.broadcast.emit('user join', '有個使用者進入了聊天室，跟他說聲 Hi 吧')
+    io.emit('online user count', online_user_count)
 
     socket.on('disconnect', function () {
+        online_user_count -= 1
         socket.broadcast.emit('user exit', '有個使用者離開了聊天室，我們懷念他')
+        io.emit('online user count', online_user_count)
     });
 
     socket.on('chat message', function (msg) {
