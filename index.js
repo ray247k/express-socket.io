df5a50ad343127f0e67de2e4547f706a98a129e3
Ray <ray247k@gmail.com>
2020-02-15 17:39:58 +0800

    Add broadcast when new user connect or disconnect

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+var app = require('express')()
+var http = require('http').createServer(app)
+var io = require('socket.io')(http)
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/view/index.html');
+    res.sendFile(__dirname + '/view/index.html')
 });
 
 io.on('connection', function (socket) {
-    console.log('a user connected');
+    socket.broadcast.emit('user join', '有個使用者進入了聊天室，跟他說聲 Hi 吧')
 
     socket.on('disconnect', function () {
-        console.log('user disconnected');
+        socket.broadcast.emit('user exit', '有個使用者離開了聊天室，我們懷念他')
     });
 
     socket.on('chat message', function (msg) {
-        console.log('message: ' + msg);
-        io.emit('chat message', msg);
+        console.log('message: ' + msg)
+        io.emit('chat message', msg)
     });
 });
 
 http.listen(3000, function () {
-    console.log('express service on, listening on :3000');
+    console.log('express service on, listening on :3000')
 });
