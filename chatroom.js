93ac3f16ac03959ba8b63c0ce7e15169d07d929e
Ray <ray247k@gmail.com>
2020-03-01 11:41:21 +0800

    Add chatroom sending and getting message function
    加入聊天室接收 / 發送訊息功能

--- chatroom.js
+++ chatroom.js
@@ -7,11 +7,18 @@ app.get('/:room_name', function (req, res) {
 });
 
 io.on('connection', function (socket) {
+    
+    // 客戶端加入聊天室
     socket.on('join room', (room_name) => {
         socket.join(room_name)
         io.to(room_name).emit('new user', `有個使用者進入了聊天室 ${room_name} 跟他說聲 Hi 吧`)
     })
-    
+
+    // 接收客戶端訊息
+    socket.on('chat message', ({room_name, msg}) => {
+        console.log(`get message from room name '${room_name}' msg = ${msg}`)
+        io.to(room_name).emit('chat message', msg)
+    })
 })
 
 
